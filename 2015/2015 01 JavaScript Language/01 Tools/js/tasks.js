4e92cf53cd8a0fa5ea303ef089ba4315e6a3b1c3
alvinlau117 <yathay@live.hk>
2016-02-25 08:14:21 +0000

    update wk 1 files

--- 2015/2015 01 JavaScript Language/01 Tools/js/tasks.js
+++ 2015/2015 01 JavaScript Language/01 Tools/js/tasks.js
@@ -18,26 +18,39 @@ function addTask() {
     // For the output:
     var message = '';
 
-    if (task.value) {
+    if(!checkDuplicates(tasks,task)){
+        if (task.value) {
+        
+            // Add the item to the array:
+            tasks.push(task.value);
+            
+            // Update the page:
+            message = '<h2>To-Do</h2><ol>';
+            for (var i = 0, count = tasks.length; i < count; i++) {
+                message += '<li>' + tasks[i] + '</li>';
+            }
+            message += '</ol>';
+            output.innerHTML = message;
+            
+        } // End of task.value IF.
     
-        // Add the item to the array:
-        tasks.push(task.value);
-        
-        // Update the page:
-        message = '<h2>To-Do</h2><ol>';
-        for (var i = 0, count = tasks.length; i < count; i++) {
-            message += '<li>' + tasks[i] + '</li>';
-        }
-        message += '</ol>';
-        output.innerHTML = message;
-        
-    } // End of task.value IF.
-
-    // Return false to prevent submission:
-    return false;
+        // Return false to prevent submission:
+        return false;
+    }else{
+        alert('The list already have what you entered!')
+        return false;
+    }
     
 } // End of addTask() function.
 
+function checkDuplicates(array,item){
+    for(i=0;i<array.length; i++){
+        if(item=array[i]){
+            return true;
+        }
+    }
+    return false;
+}
 // Initial setup:
 function init() {
     'use strict';
